54a412776477910354ff3a3d5f16c78903a105ad
Nahuel Galego <galego@ort.edu.uy>
2025-11-13 10:14:42 -0300

    Seccion 2: clase 18 - Barra de Navegacion

--- first-steps/app/(general)/layout.tsx
+++ first-steps/app/(general)/layout.tsx
@@ -1,12 +1,17 @@
-export default function AboutLayout({
+import Navbar from "@/components/navbar/Navbar";
+
+export default function GeneralLayout({
     children
 }:{
     children: React.ReactNode;
 }){
     return (
+        <>
+        <Navbar />
         <div>
             <h1>Hello Root Layout About</h1>
             {children}
         </div>
+        </>
     )
 };
