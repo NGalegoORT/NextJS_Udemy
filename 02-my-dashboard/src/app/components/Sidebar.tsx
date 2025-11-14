7cadf3779357cd5f5bfdc9ad5482012792a46291
Nahuel Galego <galego@ort.edu.uy>
2025-11-14 12:31:04 -0300

    Seccion 5: Clase 50 - Creacion de Pagina de Pokemones

--- 02-my-dashboard/src/app/components/Sidebar.tsx
+++ 02-my-dashboard/src/app/components/Sidebar.tsx
@@ -20,6 +20,25 @@ const menuItems = [
           </svg>,
     title: 'Counter',
     subTitle: 'Estado Local'
+  },
+  {
+    path:'/dashboard/pokemon',
+    icon: <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+            >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 12h6m6 0h6m-9 9a9 9 0 100-18 9 9 0 000 18zm0-6.75a3.75 3.75 0 110-7.5 3.75 3.75 0 010 7.5z"
+            />
+          </svg>,
+    title: 'Pokemon',
+    subTitle: 'Aqui estan todos los pokemon'
   }
 
 ];
---
+++ 02-my-dashboard/src/app/dashboard/pokemon/page.tsx
@@ -0,0 +1,12 @@
+
+const PokemonPage = () => {
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Pokemon Page</span>
+    </div>
+    </>
+  )
+}
+
+export default PokemonPage
