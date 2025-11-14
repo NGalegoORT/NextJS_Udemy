50ce125c4fe96137475c83d1f48c66fe5bdd3b35
Nahuel Galego <galego@ort.edu.uy>
2025-11-14 12:10:36 -0300

    Seccion 4: Clase 44 - metatags y componentes separados de Counter

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,23 +1,17 @@
-'use client'
-import { useState } from "react";
+import CartCounter from "@/app/shopping-cart/components/CartCounter";
+
+export const metadata = {
+  title: 'Shopping Cart',
+  description: 'Un simple contador',
+};
 
 const CounterPage = () => {
-  const [count, setCount] = useState(5);
 
   return (
     <>
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Productos en el Carrito</span>
-      <span className="text-9xl">{count}</span>
-    
-    <div className="flex">
-      <button onClick={()=> setCount(count - 1)} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"> 
-        -1
-      </button>
-            <button onClick={() => setCount(count + 1)} className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"> 
-        +1
-      </button>
-    </div>
+      <CartCounter value={15}/>
     </div>
     </>
   )
