5e11be9be6753d56902c2071c641e0cdfdbeb9a4
Nahuel Galego <galego@ort.edu.uy>
2025-11-14 11:20:04 -0300

    Seccion 4: Clase 42 - Solucion del Ejercicio

--- 02-my-dashboard/src/app/components/SideBarMenuItems.tsx
+++ 02-my-dashboard/src/app/components/SideBarMenuItems.tsx
@@ -13,15 +13,15 @@ interface Props{
 const SideBarMenuItems = ({path, icon, title, subTitle}: Props) => {
 
   const pathName = usePathname();
-  console.log('Pathname:', pathName);
+  //console.log('Pathname:', pathName);
   return (
     <>
-            <Link  
-                className={`mr-2 transition-all hover:underline hover:text-blue-500 
-              ${(pathName === path) && 'text-blue-700'}`}
-              href={path}>
+            <Link  href={path}
+            className={`
+              mr-2 hover:underline hover:text-blue-500 w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 hover:bg-white/5 ease-linear duration-150
+              ${(pathName === path) ? 'bg-blue-800': ''}
+              `}>
               <div>
-                
                 {icon}
 
               </div>
