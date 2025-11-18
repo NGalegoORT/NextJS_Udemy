5e31142a1b044ca7e69c069813956d57b5fcd9c5
Nahuel Galego <galego@ort.edu.uy>
2025-11-18 14:40:20 -0300

    Seccion 5: Clase 58 - Cargar informacion del Pokemon por ID

--- 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,17 +1,27 @@
+import { Pokemon } from "@/pokemons";
 interface Props {
-    // `params` can be a Promise in some Next.js setups (concurrent rendering).
-    // Allow either the direct object or a Promise so we can `await` it safely.
-    params: { id: string } | Promise<{ id: string }>;
+    params: { id: string };
+}
+
+const getPokemon = async (id: string): Promise<Pokemon> =>{
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,{
+        cache:'force-cache'
+    }).then(resp =>resp.json());
+    return pokemon;
 }
 
 export default async function PokemonPage({ params }: Props) {
     // unwrap params if Next.js passed a Promise
     const { id } = await params;
 
+    const pokemon =  await getPokemon(id)
+
+
     return (
         <>
             <div className="flex flex-col">
                 <h1>Pokemon {id}</h1>
+                <div>{JSON.stringify(pokemon)}</div>
             </div>
         </>
     );
