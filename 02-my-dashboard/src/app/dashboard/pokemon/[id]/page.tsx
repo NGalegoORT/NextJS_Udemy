1af7d1aebf59cdc56f9033088639cb2b7a556059
Nahuel Galego <galego@ort.edu.uy>
2025-11-18 16:40:00 -0300

    Seccion 6: Clase 68 - Introduccion a la nueva seccion y boton para volver

--- 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { Pokemon } from "@/pokemons";
 import { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface Props {
@@ -43,7 +44,18 @@ export default async function PokemonPage({ params }: Props) {
     <div className="flex mt-5 flex-col items-center text-slate-800">
       <div className="relative flex flex-col items-center rounded-[20px] w-[700px] mx-auto bg-white bg-clip-border  shadow-lg  p-3">
         <div className="mt-2 mb-8 w-full">
-          <h1 className="px-2 text-xl font-bold text-slate-700 capitalize">
+
+        <div className="mt-5">
+        <Link
+        href={`/dashboard/pokemons`}
+        className="border rounded-full py-2 px-4 text-xs font-semibold text-black-100"
+        >
+        Volver al listado!
+        </Link>
+        </div>
+
+
+          <h1 className="pt-2 px-2 text-xl font-bold text-slate-700 capitalize">
             #{pokemon.id} {pokemon.name}
           </h1>
           <div className="flex flex-col justify-center items-center">
