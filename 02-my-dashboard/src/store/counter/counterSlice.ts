5ce5fdb2c65bd3e7fce661abe9abc24217bd56ef
Nahuel Galego <galego@ort.edu.uy>
2025-11-20 15:07:08 -0300

    Seccion 7: Clase 84 - Server Client State

--- 02-my-dashboard/src/store/counter/counterSlice.ts
+++ 02-my-dashboard/src/store/counter/counterSlice.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface CounterState{
     count: number,
+    isReady: boolean,
 }
 
 const initialState: CounterState = {
     count: 5,
+    isReady: false,
 
 }
 
@@ -14,6 +16,13 @@ const counterSlice = createSlice({
   initialState,
   reducers: {
     
+    initCounterState(state, action: PayloadAction<number>){
+      if(state.isReady) return;
+
+      state.count = action.payload;
+      state.isReady = true;
+    },
+
     addOne(state){
       state.count++;
     },
@@ -32,6 +41,6 @@ const counterSlice = createSlice({
   }
 });
 
-export const { addOne, substractOne, resetCount } = counterSlice.actions
+export const { addOne, substractOne, resetCount, initCounterState } = counterSlice.actions
 
 export default counterSlice.reducer
