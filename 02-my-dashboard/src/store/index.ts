421aa10387fc5cc9aaa7c816da85005c2127a09f
Nahuel Galego <galego@ort.edu.uy>
2025-11-20 12:34:55 -0300

    Seccion 7: Clase 83 - Counter Reducer y Acciones

--- 02-my-dashboard/src/store/index.ts
+++ 02-my-dashboard/src/store/index.ts
@@ -1,14 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter/counterSlice'
+import { TypedUseSelectorHook,useSelector, useDispatch } from 'react-redux';
 
 export const store = configureStore({
   reducer: {
-    counterReducer,
+    counter: counterReducer,
     
   },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
